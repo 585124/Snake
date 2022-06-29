9a42beb18d19826b38d2b89f732f23f23d126e13
585124 <69625780+585124@users.noreply.github.com>
2022-06-29 15:22:32 +0200

    Add files via upload

---
+++ Snake/src/SnakeGame.java
@@ -0,0 +1,17 @@
+/*
+******************************
+******************************
+* FERDIG SPILL!!!
+******************************
+******************************
+ */
+
+
+public class SnakeGame {
+    public static void main(String[] args) {
+
+        new GameFrame();
+
+
+    }
+}
